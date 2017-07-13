2fa83d39e8c619749dc570246dfcfd7d1a0e58f6
jorge debian <jpinedom@hotmail.com>
2017-07-12 19:26:46 -0500

    add route /

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,8 @@
   |
  */
 
-Route::get('/', function () {
-    return view('auth.login');
-//    return view('welcome');
-});
-
 Auth::routes();
-
+Route::get('/', 'HomeController@index');
 Route::get('/home', 'HomeController@index');
 Route::resource('/courses', 'Administration\CoursesController');
 Route::resource('/parameters', 'Administration\ParametersController');
