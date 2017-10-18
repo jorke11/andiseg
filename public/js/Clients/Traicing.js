5c12a7ce494dc62a8af94d110cd07aaae8a62119
jorge debian <jpinedom@hotmail.com>
2017-10-18 07:28:26 -0500

    add improve

--- public/js/Clients/Traicing.js
+++ public/js/Clients/Traicing.js
@@ -1154,6 +1154,7 @@ console.log(dataOrder);
                 $('#myTabs a[href="#manager"]').tab('show');
                 $(".input-biografic").setFields({data: data});
                 dataOrder = data;
+                console.log(dataOrder);
 //                if (data.status_id == 3) {
 //                    $("#btnOkBiografic,#btnSaveBiografic").attr("disabled", true);
 //                    $(".input-biografic").setFields({data: data, disabled: true});
